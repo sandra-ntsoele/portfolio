4810edfce73fc6f00d5eea2d676e860cd87e3a5c
Hloni Ntsoele <sandra.ntsoele@pm.me>
2021-11-24 04:48:45 +0200

    created the about-me component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from "./components/Header";
+import AboutMe from "./components/AboutMe";
 
 function App() {
   return (
     <>
       <Header />
+      <AboutMe />
     </>
   );
 }
---
+++ src/components/AboutMe.js
@@ -0,0 +1,17 @@
+function AboutMe () {
+   return (
+      <section className="col-6">
+         <p className="mt-5">
+            I am an aspiring 'self taught' developer. Who first learned the basics of programming with Java but quickly moved away from that after developing an interest in web development.
+         </p>
+         <p>
+            I started learning HTML/CSS/JavaScript, created a few horrendous looking websites and later on a friend introduced to Node.js/express and a little MongoDB.
+         </p>
+         <p>
+            More recently I have been learning the .NET stack as an attempt to get the necessary skills to land a dev job opportunity and achieve the goal of being a full-stack developer.
+         </p>
+      </section>
+   );
+}
+
+export default AboutMe;
