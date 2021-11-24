9c4d3648c45b1cf654d3134b24c39b38a31a7e54
Hloni Ntsoele <sandra.ntsoele@pm.me>
2021-11-24 15:26:49 +0200

    Added functionality to dark mode switch

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
-function Header () {
+function Header ()
+{
    return (
       <header className="row pt-5 ">
 
@@ -8,12 +9,35 @@ function Header () {
          </h2>
          <div className="form-check form-switch col-4 d-grid">
             <span className="fs-6 d-none d-md-block text-end">Dark Mode</span>
-            <input className="form-check-input ms-auto" type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
+            <input className="form-check-input ms-auto" type="checkbox" role="switch" id="flexSwitchCheckDefault" onClick={DarkMode}/>
             <label className="form-check-label" htmlFor="flexSwitchCheckDefault"></label>
          </div>
 
       </header>
    );
+}
+
+function DarkMode (props)
+{
+   const body = document.body;
+   const darkTheme = "dark";
+   const lightTheme = "light";
+   let theme = localStorage.getItem("theme");
+
+   if (theme === "light")
+   {
+      // set to light
+      body.classList.remove(theme);
+      body.classList.add(darkTheme);
+
+      localStorage.setItem("theme", darkTheme);
+   } else
+   {
+      body.classList.remove(theme);
+      body.classList.add(lightTheme);
+
+      localStorage.setItem("theme", lightTheme);
+   }
 }
 
 export default Header;
