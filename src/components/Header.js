935139064590d6592e2aee86837b6fbee7bcfb39
Hloni Ntsoele <sandra.ntsoele@pm.me>
2021-11-24 04:14:40 +0200

    Created header component

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+function Header () {
+   return (
+      <header className="row pt-5 ">
+
+         <h2 className="col-8 d-grid">
+            Sandra Ntsoele
+            <span className="fs-6">Hardworking. Overthinker</span>
+         </h2>
+         <div className="form-check form-switch col-4 d-grid">
+            <span className="fs-6 d-none d-md-block">Dark Mode</span>
+            <input className="form-check-input m-0" type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
+            <label className="form-check-label" htmlFor="flexSwitchCheckDefault"></label>
+         </div>
+
+      </header>
+   );
+}
+
+export default Header;
