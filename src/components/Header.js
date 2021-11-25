acb3d6ddac8c1eda1235e5f15006898c7c69796f
Sandra <sandrantsoele@pm.me>
2021-11-25 17:10:45 +0200

    fixed dark mode toggler to reflect the correct state on load as well make use of react hooks to manipulate the element

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,59 @@
-function Header ()
+import React, { useEffect, useRef } from 'react';
+
+export default function Header ()
 {
+   // Functions used to control the dark mode toggler
+   var DarkMode = 
+   {
+      DarkModeCheck : function (toggler)
+      {
+         let theme = localStorage.getItem("theme");
+      
+         if(theme)
+         {
+            if (theme === "dark")
+            {
+               document.body.classList.add("dark");
+               toggler.current.setAttribute("checked", true);
+            }else
+            {
+               document.body.classList.remove("dark");
+            }
+         }else
+         {
+            localStorage.setItem("theme", "light");
+         }
+      },
+      DarkModeToggler : function ()
+      {
+         const body = document.body;
+         const darkTheme = "dark";
+         const lightTheme = "light";
+         let theme = localStorage.getItem("theme");
+
+         if (theme === "light")
+         {
+            body.classList.remove(theme);
+            body.classList.add(darkTheme);
+
+            localStorage.setItem("theme", darkTheme);
+         } else
+         {
+            body.classList.remove(theme);
+            body.classList.add(lightTheme);
+
+            localStorage.setItem("theme", lightTheme);
+         }
+      }
+   }
+
+   var inputRef = useRef();
+
+   useEffect(() => {
+      DarkMode.DarkModeCheck(inputRef);
+   })
+
+   
    return (
       <header className="row pt-5 ">
 
@@ -9,35 +63,9 @@ function Header ()
          </h2>
          <div className="form-check form-switch col-4 d-grid">
             <span className="fs-6 d-none d-md-block text-end">Dark Mode</span>
-            <input className="form-check-input ms-auto" type="checkbox" role="switch" id="flexSwitchCheckDefault" onClick={DarkMode}/>
-            <label className="form-check-label" htmlFor="flexSwitchCheckDefault"></label>
+            <input ref={inputRef} className="form-check-input ms-auto" type="checkbox" role="switch" id="DarkModeToggler" onClick={DarkMode.DarkModeToggler}/>            
+            <label className="form-check-label" htmlFor="DarkModeToggler"></label>
          </div>
-
       </header>
    );
 }
-
-function DarkMode (props)
-{
-   const body = document.body;
-   const darkTheme = "dark";
-   const lightTheme = "light";
-   let theme = localStorage.getItem("theme");
-
-   if (theme === "light")
-   {
-      // set to light
-      body.classList.remove(theme);
-      body.classList.add(darkTheme);
-
-      localStorage.setItem("theme", darkTheme);
-   } else
-   {
-      body.classList.remove(theme);
-      body.classList.add(lightTheme);
-
-      localStorage.setItem("theme", lightTheme);
-   }
-}
-
-export default Header;
